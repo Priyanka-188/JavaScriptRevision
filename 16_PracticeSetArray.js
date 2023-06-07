db220bb12407de26de901ad9875ec6276304c129
Priyanka-191925 <sainipriyanka1722001@gmail.com>
2023-06-07 23:21:21 +0530

    16

--- 16_PracticeSetArray.js
+++ 16_PracticeSetArray.js
@@ -1,12 +1,16 @@
+//WAP to take input from the user and update the array with the element
 const prompt = require('prompt-sync')();
-let arr = [1]
-console.log(arr)
+let arr = [1];
+console.log(arr);
 let size = prompt("Enter the size of array : ");
 console.log("Enter the elements of array ")
-for(let i=0;i<size;i++)
+for(let i=0;i<size;i++);
 {
    let element = prompt("Enter "+ i+"th "+"element ")
    element = Number.parseInt(element)
    arr.push(element)
 }
-console.log(arr)
+console.log(arr);
+
+//Question 2
+console.log("Hello this is programming")
