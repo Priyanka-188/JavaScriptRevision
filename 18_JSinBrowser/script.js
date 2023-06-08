549594c81446f2a8ba650ddfaed2c740b01c19f1
Priyanka-191925 <sainipriyanka1722001@gmail.com>
2023-06-08 19:14:52 +0530

    practiceset

--- 18_JSinBrowser/script.js
+++ 18_JSinBrowser/script.js
@@ -1,11 +1,22 @@
-alert("Enter a value!")
-let value = prompt("Enter a value here")
+// alert("Enter a value!")
+// let value = prompt("Enter a value here")
 
 
-let res=confirm("Do you want to print it ? ")
-if(res){
-    document.write(value)
-}
-else{
-    document.write("please allow me to write here")
-}
+// let res=confirm("Do you want to print it ? ")
+// if(res){
+//     document.write(value)
+// }
+// else{
+//     document.write("please allow me to write here")
+// }
+
+//Practice set
+
+//question1 Write a program using prompt function to take input of age as a value from the user and use alert to tell him if he can drive.
+
+let age = prompt("Enter your age: ")
+age = Number.parseInt(age)
+
+alert(age>18?"You can drive!":"You are a minor so you can not drive!")
+
+//question2. In Ques1 use confirm to ask the user if he wabts ti see the prompt again
