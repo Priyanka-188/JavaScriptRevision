f00810a03a8b3789547cecb2a290f1dbab483114
Priyanka-191925 <sainipriyanka1722001@gmail.com>
2023-07-02 16:45:34 +0530

    inner and outer html

--- 19_ElementOnlyNavigation/script.js
+++ 19_ElementOnlyNavigation/script.js
@@ -5,3 +5,10 @@ let b = document.body
 console.log("First child of b is ",b.firstChild)
 console.log("First Element child of b is ",b.firstElementChild)
 
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName);
+
+//inner and outer HTMl
+first.outerHTML = "<div>Only  <b>Hare Krishna </b></div>";
+//inner HTML is valid only for element nodes.
+console.log(document.body.firstElementChild.data);
